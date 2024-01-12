673d1436d71e3f6c9bbdc72caf29255c31bcbd71
Crypto_App <mahmoudsaber223015email@gmail.com>
2024-01-12 10:03:44 +0200

    finish refactor chat

--- lib/features/chat/presentation/views/chat_view.dart
+++ lib/features/chat/presentation/views/chat_view.dart
@@ -1,20 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:social_app/core/utils/functions/show_toast.dart';
 import 'package:social_app/core/widgets/custom_circular_progress_indicator.dart';
+import 'package:social_app/core/widgets/custom_text_field.dart';
 import 'package:social_app/features/chat/presentation/widgets/appbar_chat.dart';
 import 'package:social_app/features/chat/presentation/widgets/chat_item.dart';
 import 'package:social_app/features/layout/presentation/cubit/layout_cubit.dart';
 import 'package:social_app/features/layout/presentation/cubit/layout_state.dart';
 
-class ChatView extends StatelessWidget {
+class ChatView extends StatefulWidget {
   const ChatView({super.key});
+
+  @override
+  State<ChatView> createState() => _ChatViewState();
+}
+
+class _ChatViewState extends State<ChatView> {
+  TextEditingController searchController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         AppBarChat(),
+        SizedBox(
+          height: 20.h,
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          child: SizedBox(
+            height: 50,
+            child: CustomTextField(
+              keyboardType: TextInputType.text,
+              controller: searchController,
+              hintText: "Search 'Your Friend'",
+              prefix: Icon(Icons.search),
+              suffix: Icon(Icons.mic_outlined),
+            ),
+          ),
+        ),
         BlocBuilder<LayoutCubit, LayoutState>(builder: (context, state) {
           var cubit = LayoutCubit.get(context);
           if (state is GetAllUserSuccess) {
